0b191357e933a57188ab33076442b945b933a62b
Nadia Rahman <146254492+Nadia8844@users.noreply.github.com>
2023-11-14 16:10:53 +0000

    week7sess1

--- StepsTask1/StepsTask1.c
+++ StepsTask1/StepsTask1.c
@@ -81,4 +81,3 @@ int main() {
     fclose(file);
     return 0;
 }
-
---
+++ week7sess1/empty.c
@@ -0,0 +1,9 @@
+typedef struct{
+    char date[20];
+    float bloodIron;
+} reading;
+
+int main(){
+    reading one reading;
+    reading weekly readings [7];
+}
